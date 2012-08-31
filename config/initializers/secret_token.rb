d00bb7149b77549ee895e6dede0490506ad9a538
Agis Mesolongitis <agis@Nescafe.local>
2012-08-31 14:12:59 -0400

    saas tutorial - beginning of db migrations, end of page 111

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iceapp::Application.config.secret_token = '0c41ff695e2fbf736f2ef5b8d6b83fd554cc6f0aa021d1c828008d76685b77383893eec7d3ec1a82c51db348adcedc4360d2854e464acd52542d3c6fe6299e43'
